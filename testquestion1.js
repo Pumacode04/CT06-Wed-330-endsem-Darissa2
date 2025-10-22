79246e7e71b55040296f1a69f7cf285c25ad36a6
Pumacode04 <cransonlim@yahoo.com.sg>
2025-10-22 08:23:15 +0000

    EndSem

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,23 @@
+let xpos;
+let xSpeed;
+
+function setup(){
+    createCanvas(600, 400);
+    xpos = 300;
+    xSpeed = 5;
+    background(0, 0, 200);
+    fill('limegreen');
+}
+
+function draw(){
+    background(0, 0, 200);
+    circle(xpos, 200, 50);
+    xpos = xpos + xSpeed;
+    if ( xpos === 575 ){
+        xSpeed = xSpeed * -1;
+    }
+    if ( xpos === 25 ){
+        xSpeed = xSpeed * -1;
+    }
+}
 
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,17 @@
+let pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+let yPos;
 
+function setup(){
+    pokemons = ["Pikachu","Raichu","Bulbasaur","Mewtwo","Charizard"];
+    createCanvas(200, 300);
+    fill("pink");
+    textAlign(CENTER, CENTER)
+    textSize(20);
+    // Task: Align the text to the center using textAlign
+    // Task: set font size to 20 using textSize
+    yPos = 50;
+    for (i=0; i<5; i++) {
+        text(pokemons[i], 100, yPos);
+        yPos = yPos + 50;
+    }
+}
